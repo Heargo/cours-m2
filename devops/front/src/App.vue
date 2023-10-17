7c94f990f00ab0c3201bb58eeaf2f6e6e95347a8
Hugo REY <hrey222@gmail.com>
2023-10-17 14:22:25 +0200

    feat: cleanup front

--- devops/front/src/App.vue
+++ devops/front/src/App.vue
@@ -1,93 +1,52 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-
-import { useApiStore } from './stores/api'
-
-const apiStore = useApiStore()
-
-apiStore.getRandomFortune().then((quote) => {
-  console.log(quote)
-})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+  <div class="content">
+    <header>
+      <img alt="Vue logo" class="logo" src="@/assets/dice.png" width="125" height="125" />
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Fortune</RouterLink>
+        <RouterLink to="/astrology">Astrology</RouterLink>
       </nav>
-    </div>
-  </header>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.content {
+  width: 80%;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: row;
+  gap: 3rem;
+}
+
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
   nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+    a {
+      color: var(--text);
+      &.router-link-exact-active {
+        color: hsla(160, 100%, 37%, 1);
+      }
+      &.router-link-exact-active:hover {
+        background-color: transparent;
+      }
+    }
   }
 }
 </style>
